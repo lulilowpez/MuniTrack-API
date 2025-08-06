bd0c57b383df4c48b6755ccd347550f4a2a4f4be
evecanton <cantonevelynr@gmail.com>
2025-08-05 20:34:29 -0600

    add context

---
+++ Infrastructure/MuniDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class MuniDbContext : DbContext
+    {
+        public MuniDbContext()
+        {
+
+        }
+
+        public MuniDbContext(DbContextOptions<MuniDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
